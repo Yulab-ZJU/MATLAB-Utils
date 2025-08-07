07b0deaf25f14c2a189296d41d8678309e1a3b45
TOMORI233 <ihatewo@qq.com>
2025-08-07 15:44:48 +0800

    update 2025-08-07 15:44:46 by tomori

--- initMATLABUtils.m
+++ initMATLABUtils.m
@@ -5,8 +5,3 @@ addpath(genpath(fullfile(rootpath, "external")));
 addpath(genpath(fullfile(rootpath, "toolbox API")));
 addpath(genpath(fullfile(rootpath, "user interface")));
 addpath(fullfile(rootpath, "callback"));
-
-if ~exist(fullfile(rootpath, "resources", "functionSignatures.json"), "file")
-    mkdir(fullfile(rootpath, "resources"));
-    copyfile(fullfile(rootpath, "docs", "functionSignatures.json"), fullfile(rootpath, "resources", "functionSignatures.json"));
-end
