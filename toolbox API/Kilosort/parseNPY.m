3bb387e6f64d65b90b30dbb1a13ce58b1a70a5b0
TOMORI233 <ihatewo@qq.com>
2025-08-28 14:34:29 +0800

    update 2025-08-28 14:34:29 by tomori

--- toolbox API/Kilosort/parseNPY.m
+++
@@ -1,8 +0,0 @@
-function [spikeIdx, clusterIdx, templates, spikeTemplateIdx] = parseNPY(ROOTPATH)
-spikeIdx = readNPY([ROOTPATH, '\spike_times.npy']);
-clusterIdx = readNPY([ROOTPATH, '\spike_clusters.npy']);
-templates = readNPY([ROOTPATH, '\templates.npy']);
-spikeTemplateIdx = readNPY([ROOTPATH, '\spike_templates.npy']);
-
-return;
-end
