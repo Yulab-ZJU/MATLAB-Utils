dae497579e6a792fce39119bea09619870435dd0
TOMORI233 <ihatewo@qq.com>
2025-10-27 15:48:13 +0800

    update 2025-10-27 15:48:13 by xhx

--- data processing/discrete/spike/plot/mu_plotNoiseResponse.m
+++ data processing/discrete/spike/plot/mu_plotNoiseResponse.m
@@ -1,12 +1,21 @@
-function varargout = plotRaster(trialAll, window, clus)
-narginchk(2, 3);
+function varargout = mu_plotNoiseResponse(trialAll, window, clus)
+narginchk(1, 3);
+
+if nargin < 2
+    window = [-100, 500];
+end
 
 if nargin < 3
     clus = [];
 end
 
-spikes = {trialAll.spike}';
-if ~isempty(clus)
+if isstruct(trialAll)
+    spikes = {trialAll.spike}';
+elseif iscell(trialAll)
+    spikes = trialAll;
+end
+
+if ~isempty(clus) && all(cellfun(@(x) size(x, 2), spikes) == 2)
     spikes = cellfun(@(x) x(x(:, 2) == clus, 1), spikes, "UniformOutput", false);
 else
     spikes = cellfun(@(x) x(:, 1), spikes, "UniformOutput", false);
@@ -23,7 +32,7 @@ yticklabels(ax1, '');
 ax2 = mu.subplot(Fig, 1, 1, 1, [1/2, 1/3], "alignment", "center-bottom");
 [psth, edges] = mu_calPSTH(spikes, window, 10, 5);
 plot(ax2, edges, psth, "Color", "k", "LineWidth", 2);
-xlabel(ax2, "Time from trial onset (ms)");
+xlabel(ax2, "Time from noise onset (ms)");
 ylabel(ax2, "Firing rate (Hz)");
 
 mu.scaleAxes(Fig, "x", window);
@@ -31,9 +40,14 @@ mu.addLines(Fig, struct("X", 0));
 mu.scaleAxes(Fig, "x", window);
 mu.addLines(Fig, struct("X", 0));
 
-if nargout == 1
-    varargout{1} = Fig;
+latency = mu_calLatency(spikes, [0, 300], [-100, 0]);
+if ~isempty(latency)
+    mu.addLines(Fig, struct("X", latency, "color", "r"));
+    title(ax1, ['Latency for onset response: ', num2str(latency), ' ms']);
+else
+    title(ax1, 'No significant onset response found');
 end
+
 if nargout == 1
     varargout{1} = Fig;
 end
