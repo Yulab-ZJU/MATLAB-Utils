5c35d67ecf1b3760eb6cca6c16fc6f367ccf8a30
TOMORI233 <ihatewo@qq.com>
2025-10-27 15:48:17 +0800

    update 2025-10-27 15:48:17 by tomori

--- +mu/addWaveError.m
+++ +mu/addWaveError.m
@@ -2,8 +2,8 @@ function varargout = addWaveError(varargin)
 %ADDWAVERROR  Add shaded area around a time series curve.
 %
 % SYNTAX:
-%     mu.addWaveError(t, Y, E, [C])
-%     mu.addWaveError(ax, t, Y, E, [C])
+%     mu.addWaveError(t, Y, E)
+%     mu.addWaveError(ax, t, Y, E, "Color", rgb, "Alpha", alpha)
 %     h = mu.addWaveError(...)
 %
 % INPUTS:
@@ -13,17 +13,25 @@ function varargout = addWaveError(varargin)
 %     E   - error value vector
 %   OPTIONAL:
 %     ax     - target axes (default=gca)
-%     C      - Color (default='k')
+%   NAMEVALUE:
+%     Color  - Color variable (default='k')
 %     Alpha  - Transparency. 0 for completely transparent, 1 for opaque (default=0.3)
 %
 % OUTPUTS:
 %     h  - patch object
 
-if isgraphics(varargin{1}(1), "axes")
-    ax = varargin{1}(1);
-    varargin = varargin(2:end);
+if isgraphics(varargin{1}, "axes")
+    ax = varargin{1};
+    t  = varargin{2};
+    Y  = varargin{3};
+    E  = varargin{4};
+    varargin(1:4) = [];
 else
     ax = gca;
+    t  = varargin{1};
+    Y  = varargin{2};
+    E  = varargin{3};
+    varargin(1:3) = [];
 end
 
 mIp = inputParser;
@@ -31,14 +39,14 @@ mIp.addRequired("ax", @(x) isgraphics(x, "axes"));
 mIp.addRequired("t", @(x) validateattributes(x, 'numeric', {'vector', 'increasing'}));
 mIp.addRequired("Y", @(x) validateattributes(x, 'numeric', {'vector', 'real'}));
 mIp.addRequired("E", @(x) validateattributes(x, 'numeric', {'vector', 'real'}));
-mIp.addOptional("C", "k");
-mIp.addOptional("Alpha", 0.3, @(x) validateattributes(x, 'numeric', {'scalar', 'positive', '<=', 1}));
-mIp.parse(ax, varargin{:});
+mIp.addParameter("Color", "k");
+mIp.addParameter("Alpha", 0.3, @(x) validateattributes(x, 'numeric', {'scalar', 'positive', '<=', 1}));
+mIp.parse(ax, t, Y, E, varargin{:});
 
 t = mIp.Results.t(:)';
 Y = mIp.Results.Y;
 E = mIp.Results.E;
-C = validatecolor(mIp.Results.C);
+C = validatecolor(mIp.Results.Color);
 AlphaVal = mIp.Results.Alpha;
 
 y1 = Y(:)' + E(:)';
