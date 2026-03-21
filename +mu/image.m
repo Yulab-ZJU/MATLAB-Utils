9e90028d3a9e937e7a977a5fb5044b6316376f9f
TOMORI233 <ihatewo@qq.com>
2026-03-21 15:14:30 +0800

    debug

--- +mu/image.m
+++ +mu/image.m
@@ -1,5 +1,5 @@
 function varargout = image(varargin)
-%IMAGE  Create image like `imagesc` using `pcolor`
+%IMAGE Create image-like plot using pcolor, supporting non-uniform spacing
 %
 % SYNTAX:
 %   mu.image(I)
@@ -13,7 +13,7 @@ else
     ax = gca;
 end
 
-if isscalar(varargin)
+if numel(varargin) == 1
     I = varargin{1};
     x = 1:size(I, 2);
     y = 1:size(I, 1);
@@ -25,45 +25,45 @@ else
     error('Invalid number of inputs');
 end
 
-validateattributes(x, 'numeric', {'vector'});
-validateattributes(y, 'numeric', {'vector'});
+x = x(:)'; 
+y = y(:)';
 
-if length(x) > 1
-    dx = x(2) - x(1);
-else
-    dx = 1;
-end
-assert(all(diff(x) == dx), '[x] should be equally spaced.');
+X = calculate_bounds(x);
+Y = calculate_bounds(y);
 
-if length(y) > 1
-    dy = y(2) - y(1);
-else
-    dy = 1;
-end
-assert(all(diff(y) == dy), '[y] should be equally spaced.');
-
-X = [x - dx/2, x(end) + dx/2];
-Y = [y - dy/2, y(end) + dy/2];
 [X_grid, Y_grid] = meshgrid(X, Y);
 
-% Pad matrix
 I_padded = padarray(I, [1, 1], 0, 'post');
 
 hold_state = ishold(ax);
 h = pcolor(ax, X_grid, Y_grid, I_padded);
 
-set(h, 'EdgeColor', 'none', 'FaceColor', 'flat'); % remove grids
-set(ax, 'YDir', 'reverse');                       % match imagesc default
+set(h, 'EdgeColor', 'none', 'FaceColor', 'flat'); 
+set(ax, 'YDir', 'reverse');
 axis(ax, 'tight');
-set(ax, 'XTick', x, 'YTick', y);
+
+if length(x) < 50
+    set(ax, 'XTick', x);
+end
+if length(y) < 50
+    set(ax, 'YTick', y);
+end
 
 if ~hold_state
-    hold(ax, hold_state);
+    hold(ax, 'off');
 end
 
 if nargout > 0
     varargout{1} = h;
 end
 
-return;
+end
+
+function bounds = calculate_bounds(vec)
+    if length(vec) > 1
+        d = diff(vec);
+        bounds = [vec(1)-d(1)/2, vec(1:end-1)+d/2, vec(end)+d(end)/2];
+    else
+        bounds = [vec - 0.5, vec + 0.5];
+    end
 end
